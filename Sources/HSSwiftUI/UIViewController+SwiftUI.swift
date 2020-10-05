720677f7a5220668a9420c27b9fbd8f480a46ade
Rob Jonson <rob@hobbyistsoftware.com>
2020-10-05 10:27:13 +0100

    experiment with importing PureLayout

---
+++ Sources/HSSwiftUI/UIViewController+SwiftUI.swift
@@ -0,0 +1,38 @@
+//
+//  UIViewController+SwiftUI.swift
+//  MilkoJumps
+//
+//  Created by Rob Jonson on 27/01/2020.
+//  Copyright © 2020 HobbyistSoftware. All rights reserved.
+//
+#if canImport(UIKit) && canImport(PureLayout)
+
+
+import Foundation
+import SwiftUI
+import UIKit
+import PureLayout
+
+extension UIViewController {
+    @discardableResult
+    func insert<Content>(swiftUIView rootView:Content, inUIView container:UIView) -> UIHostingController<Content> where Content : View {
+        let childVC = UIHostingController(rootView: rootView)
+        
+        self.addChild(childVC)
+        childVC.view.frame = container.bounds
+        container.addSubview(childVC.view)
+        childVC.didMove(toParent: self)
+        
+        childVC.view.autoPinEdgesToSuperviewEdges()
+        
+        return childVC
+    }
+    
+    static func with<Content>(swiftUIView:Content) -> UIViewController where Content : View {
+        let vc = UIViewController.init(nibName: nil, bundle: nil)
+        vc.insert(swiftUIView: swiftUIView, inUIView: vc.view)
+        return vc
+    }
+}
+
+#endif
