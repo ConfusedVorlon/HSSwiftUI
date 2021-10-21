fcdcf422c7b80065f6280e872a1b63eb2c0876b3
Rob Jonson <rob@hobbyistsoftware.com>
2021-10-21 19:06:47 +0100

    Add FadeIf

---
+++ Sources/HSSwiftUI/View+Fade.swift
@@ -0,0 +1,34 @@
+//
+//  FadeIf.swift
+//  FadeIf
+//
+//  Created by Rob Jonson on 13/09/2021.
+//  Copyright © 2021 HobbyistSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+public extension View {
+
+    /// Whether the view is hidden.
+    /// - Parameter hide: Set to `true` to hide the view. Set to `false` to show the view.
+    /// - Parameter shrinkSize: size when hidden or nil to ignore
+    /// - Returns: view (or not)
+    func fadeIf(_ fade: Bool, fadedSize:CGSize? = nil) -> some View {
+        modifier(FadeIfModifier(fade: fade,fadedSize: fadedSize))
+    }
+}
+
+/// Variables can be used in place so that the content can be changed dynamically.
+private struct FadeIfModifier: ViewModifier {
+
+    fileprivate let fade: Bool
+    fileprivate let fadedSize: CGSize?
+
+    fileprivate func body(content: Content) -> some View {
+        content
+            .opacity(fade ? 0 : 1)
+            .animation(.easeInOut(duration: 0.2),value: fade)
+            .frame(width:fade ? fadedSize?.width : nil, height: fade ? fadedSize?.height : nil)
+    }
+}
