e82dae8c68ee68f42a8cc3d374bcad4d362d2421
Rob Jonson <rob@hobbyistsoftware.com>
2023-02-28 12:13:17 +0000

    remove reference to HSHelpers

--- Sources/HSSwiftUI/Button+Config.swift
+++ Sources/HSSwiftUI/Button+Config.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import HSHelpers
+
 
 /// Button - but the label viewbuilder is given the button configuration directly
 /// ```
@@ -20,14 +20,14 @@ import HSHelpers
 /// Note that you are fully responsible for the presentation, including any variation when the button is pressed / disabled
 public struct ButtonBuilder<Label:View>: View {
     
-    public init(action: @escaping Action,
+    public init(action: @escaping ()->Void,
                 label: @escaping (ButtonStyleConfiguration) -> Label) {
         self.action = action
         self.label = label
     }
     
 
-    public var action:Action
+    public var action:()->Void
     @ViewBuilder public var label: (ButtonStyleConfiguration) -> Label
       
     public var body: some View {
