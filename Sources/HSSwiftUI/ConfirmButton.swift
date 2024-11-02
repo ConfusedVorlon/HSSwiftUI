13d4b683622ce16acb435b263091d58de89a03bb
Rob Jonson <rob@hobbyistsoftware.com>
2024-11-02 12:03:21 +0000

    Add ConfirmButton

---
+++ Sources/HSSwiftUI/ConfirmButton.swift
@@ -0,0 +1,73 @@
+//
+//  ConfirmButton.swift
+//  HSSwiftUI
+//
+//  Created by Rob Jonson on 02/11/2024.
+//
+
+
+//
+//  ConfirmButton.swift
+//  FreeFallFilms
+//
+//  Created by Rob Jonson on 02/11/2024.
+//
+
+import SwiftUI
+import HSHelpers
+
+struct ConfirmButton<Label>: View where Label: View {
+    internal init(
+        confirmTitle: LocalizedStringResource,
+        confirmMessage: LocalizedStringResource,
+        confirmButtonText: LocalizedStringResource,
+        action: @escaping MainAction,
+        label: @escaping () -> Label
+    ) {
+        self.confirmTitle = confirmTitle
+        self.confirmMessage = confirmMessage
+        self.confirmButtonText = confirmButtonText
+        self.action = action
+        self.label = label
+    }
+
+    @State private var showConfirmAlert: Bool = false
+
+    let confirmTitle: LocalizedStringResource
+    let confirmMessage: LocalizedStringResource
+    let confirmButtonText: LocalizedStringResource
+    let action: MainAction
+    @ViewBuilder let label: () -> Label
+
+    var body: some View {
+        Button {
+            showConfirmAlert = true
+        } label: {
+            label()
+        }
+        .alert(isPresented: $showConfirmAlert) {
+            Alert(
+                title: Text(confirmTitle),
+                message: Text(confirmMessage),
+                primaryButton: .destructive(Text(confirmButtonText)) {
+                    action()
+                },
+                secondaryButton: .cancel()
+            )
+        }
+
+    }
+}
+
+#Preview {
+    ConfirmButton(
+        confirmTitle: "Confirm Please",
+        confirmMessage: "This will do stuff",
+        confirmButtonText: "Confirm",
+        action: {
+        }, label:
+            {
+                Text("Delete")
+            }
+    )
+}
